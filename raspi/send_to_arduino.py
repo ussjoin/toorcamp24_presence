abcd0daa97015223fc51d8d12497d32f18863fc5
Brendan Francis O'Connor <ussjoin@ussjoin.com>
2024-06-17 21:51:48 -0700

    Now parsing people.

--- raspi/send_to_arduino.py
+++ raspi/send_to_arduino.py
@@ -12,9 +12,14 @@
 #   brew services start mosquitto
 # Or, if you don't want/need a background service you can just run:
 #   /opt/homebrew/opt/mosquitto/sbin/mosquitto -c /opt/homebrew/etc/mosquitto/mosquitto.conf
+
+
+# https://pypi.org/project/paho-mqtt/
 import paho.mqtt.client as mqtt
+import yaml
+import re
 
-
+frens = re.compile("^NR7WL-[A-Z0-9]{4}$")
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
@@ -25,20 +30,33 @@ def on_connect(client, userdata, flags, reason_code, properties):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    m = frens.match(msg.payload.decode("utf-8"))
+    if m:
+        identifier = m.group()
+        if identifier in people_lookup:
+            person = people_lookup[identifier]
+            print(f"Found friend: {person['name']}")
+        else:
+            print(f"Found someone I don't know: {identifier}")
+    else:
+        print(f"Got a message incoming I didn't understand: {msg.payload.decode('utf-8')}")
+    
     # TODO:
-    # Parse payload
-    # Beacon to Person
     # Send command to Arduino
 
-# TODO:
-# parse YAML
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
 mqttc.connect("localhost", 1883, 60)
+
+people_lookup = {}
+
+with open("people.yaml", "r") as f:
+    people = yaml.safe_load(f)['users']
+    for person in people:
+        people_lookup[person['beacon']] = person
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
